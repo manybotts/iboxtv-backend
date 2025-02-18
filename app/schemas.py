274eb0a0efad9afeb2f6a978c58b2bfbc521fdef
manybotts <105394368+manybotts@users.noreply.github.com>
2025-02-18 07:46:03 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
 
 class Show(BaseModel):
-    id: int
+    id: str = None
     title: str
+    season_episode: str
     download_link: str
-    is_streamable: bool
-    popularity: int
+    poster: str
+    description: str
+    popularity: int = 0
 
     model_config = {
-        "from_attributes": True
+        "from_attributes": True  # For Pydantic v2; use "orm_mode": True for v1.
     }
