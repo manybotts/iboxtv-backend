8f6a6e1e77c3db68ef0a9fe83a725a65fe2879d2
manybotts <105394368+manybotts@users.noreply.github.com>
2025-02-18 05:12:31 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Show(Base):
     __tablename__ = "shows"
     
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True, nullable=False)
+    title = Column(String, index=True, nullable=False, unique=True)  # Unique constraint to prevent duplicates
     download_link = Column(String, nullable=False)
     is_streamable = Column(Boolean, default=False)
     popularity = Column(Integer, default=0)
